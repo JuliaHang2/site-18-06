6b96218c407c7d392981114bf82a0e049f9cb0b0
Júlia Hang <153762550+JuliaHang2@users.noreply.github.com>
2024-06-19 17:12:57 -0300

    Add files via upload

---
+++ júlia/script.js
@@ -0,0 +1,21 @@
+var teste = "teste";
+var numero1 = 2;
+var numeroA = "2";
+var palavra = "teste1";
+var numero2 = 4;
+var ex1;
+
+console.log(teste);
+
+ex1=teste+numero1;
+console.log(ex1);
+
+ex2=teste+numeroA;
+console.log(ex2);
+
+ex3=(numero1/numero2)/numeroA;
+console.log(ex3);
+
+
+
+
